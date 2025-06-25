50b09783f1bd06e17c5bb11dac3104b940b74e80
Dean Radcliffe <24406+deanrad@users.noreply.github.com>
2025-06-24 22:46:43 -0500

    starting point: app shell

---
+++ source/app.tsx
@@ -0,0 +1,105 @@
+//@ts-nocheck
+import React, { useState, useEffect } from "react";
+import { Text, Box, useStdout, useInput } from "ink";
+import TextInput from "ink-text-input";
+import { chatFx } from "./effects/chatEffect.js";
+import { useWhileMounted, useService } from "@rxfx/react";
+
+interface ChatMessageProps {
+  message: Message;
+}
+
+const banner = `
+ ,ggggggggggg,             ,gggggggggggggg
+dP"""88""""""Y8,          dP""""""88""""""
+Yb,  88      \`8b          Yb,_    88
+ \`"  88      ,8P           \`""    88
+     88aaaad8P"                ggg88gggg
+     88""""Yb,       ,gg,   ,gg   88   8,gg,   ,gg
+     88     "8b     d8""8b,dP"    88   d8""8b,dP"
+     88      \`8i   dP   ,88"gg,   88  dP   ,88"
+     88       Yb,,dP  ,dP"Y8,"Yb,,8P,dP  ,dP"Y8,
+     88        Y88"  dP"   "Y8 "Y8P'8"  dP"   "Y8
+`;
+
+export function ChatMessage({ message }: ChatMessageProps) {
+  const isUser = message.role === "user";
+  return (
+    <Box justifyContent={isUser ? "flex-end" : "flex-start"}>
+      <Text>{message.content + "\n"} </Text>
+    </Box>
+  );
+}
+
+const randomId = (length = 7) => {
+  return Math.floor(Math.pow(2, length * 4) * Math.random())
+    .toString(16)
+    .padStart(length, "0");
+};
+
+export default function App() {
+  const [query, setQuery] = useState("");
+  const { write } = useStdout();
+
+  // TODO Hook up service state as variable 'messages'
+  const messages = [];
+  // TODO distinguish loading (before response) from working (active)
+
+  useInput((_, key) => {
+    if (key.escape) {
+      // TODO suppport cancelation
+    }
+
+    if (key.upArrow) {
+      setQuery("Who is Sam Altman?"); // just an example
+    }
+  });
+
+  function handleSubmit(value) {
+    const userMessage = {
+      id: randomId(),
+      content: value,
+      role: "user",
+      createdAt: new Date(),
+    };
+    setQuery("");
+
+    // TODO send userMessage to service
+  }
+
+  return (
+    <Box flexDirection="column">
+      <Box>
+        <Text>{banner}</Text>
+      </Box>
+      <Box flexDirection="column">
+        {messages.map((message, idx) => (
+          <ChatMessage key={idx} message={message} />
+        ))}
+      </Box>
+      <Box>
+        <Text>
+          {/* TODO display a loading state */}
+          Ask the <Text bold>AI</Text>{" "}
+          <Text dimColor> (Esc to cancel, Ctrl-C to quit)</Text>:
+        </Text>
+      </Box>
+      <Box flexDirection="row" borderStyle="double">
+        <Text> &gt; </Text>
+        <TextInput
+          value={query}
+          onChange={(v) => {
+            // TODO Disable typing while working
+            setQuery(v);
+          }}
+          onSubmit={(value) => {
+            // TODO Disable typing while working
+            handleSubmit(value);
+          }}
+          width={50}
+          alignSelf="auto"
+        ></TextInput>
+      </Box>
+    </Box>
+  );
+}
---
+++ source/effects/chatEffect.ts
@@ -0,0 +1,150 @@
+// @ts-nocheck
+import {
+  createEffect,
+  Observable,
+  after,
+  THRESHOLD,
+  randomizePreservingAverage,
+} from "rxfx";
+import OpenAI from "openai";
+import { produce } from "immer";
+
+// #region Types
+export type MessageRole = "user" | "assistant" | "system";
+
+export interface Message {
+  id: string;
+  content: string;
+  role: MessageRole;
+  createdAt: Date;
+  isComplete?: boolean;
+}
+export interface UserMessage extends Message {
+  role: "user";
+}
+export interface AssistantMessage extends Message {
+  role: "assistant";
+}
+
+export interface Chunk {
+  requestId: string;
+  text: string;
+}
+
+export interface SuggestionCard {
+  id: string;
+  title: string;
+  content: string;
+}
+// #endregion
+
+// #region OpenAI API
+let openai;
+
+function getAPIKey() {
+  if (typeof window !== "undefined") {
+    // @ts-expect-error
+    return window.OPENAI_API_KEY;
+  } else {
+    return process.env["OPENAI_API_KEY"];
+  }
+}
+// #endregion
+
+// TODO Bonus: Introduce a delay after which each token is printed
+export const chatFx = createEffect<UserMessage, Chunk, Error, Message[]>(
+  getLLMStream
+);
+
+// Use the reducer to populate chatRxFxService.state
+chatFx.reduceWith(
+  produce((messages, event) => {
+    if (event.type === "request") {
+      const userMessage = event.payload;
+      const origId = "" + userMessage.id;
+
+      // create placeholder
+      const assistantMessage: AssistantMessage = {
+        id: origId,
+        content: "",
+        role: "assistant",
+        createdAt: new Date(),
+        isComplete: false,
+      };
+
+      // prefix only the request in state, so updates find the response
+      messages.push({ ...userMessage, id: `req-${origId}` });
+      messages.push(assistantMessage);
+    }
+    if (event.type === "response") {
+      const chunk = event.payload;
+      const response = messages.find(
+        (m) => m.id === chunk.requestId && m.role === "assistant"
+      );
+      response.content += chunk.text;
+    }
+
+    if (event.type === "canceled") {
+      const response = messages.find(
+        (m) => m.id === event.payload.id && m.role === "assistant"
+      );
+      response.content += " (Canceled)";
+    }
+
+    return messages;
+  }),
+  [] // initial value - lost way down below :o
+);
+
+// Definitions
+function getLLMStream(userMessage: UserMessage): Observable<Chunk> {
+  try {
+    openai ||= new OpenAI({
+      apiKey: getAPIKey(),
+      dangerouslyAllowBrowser: true,
+    });
+  } catch (error) {
+    console.error("Failed to initialize OpenAI:", error);
+  }
+
+  return new Observable((notify) => {
+    let canceled = false; // in order to truly stop streaming
+
+    openai.chat.completions
+      .create({
+        model: "gpt-4.1",
+        messages: [
+          // TODO include previous messages when sending the current one
+          {
+            role: "user",
+            content: userMessage.content,
+          },
+        ],
+        stream: true,
+      })
+      .then(async (stream) => {
+        for await (const chunk of stream) {
+          const { delta } = chunk.choices[0];
+
+          if (!delta.content) continue;
+
+          // If we don't break on cancelation - we will still be consuming the network response,
+          // though the UI won't show it. Cancel responsibly.
+          if (canceled) break;
+
+          // notify.next args become a piece of Observable output
+          notify.next({
+            requestId: userMessage.id,
+            text: delta.content,
+          });
+        }
+        // Always complete - since .next doesn't complete by itself, unlike Promise.resolve
+        notify.complete();
+      });
+
+    return () => {
+      canceled = true;
+    };
+  });
+}
+// #endregion
