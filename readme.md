fb9fd9a487d191e37103f09efb5899f9adf6bc56
Dean Radcliffe <24406+deanrad@users.noreply.github.com>
2025-06-24 22:46:43 -0500

    starting point: app shell

---
+++ readme.md
@@ -0,0 +1,27 @@
+# Conversational UX with RxFx
+
+An example app of how RxFx can enable simpler building of Conversational UX apps, using the style of ChatGPT, and featuring streaming, cancelation, and activity detection, building mostly in the React-Free Zone.
+
+## Branches:
+
+- `demo-start` - An empty app shell with TODO comments
+- `demo-finish` - The finished version with all features
+
+## Steps to build:
+
+1. Send requests, get streaming updates
+2. Add Loading/Active states
+3. Support Cancelation, Reset
+4. Handle errors, display logs
+5. Block new requests while active
+6. Adjust timing of printed chunks for realism (bonus!)
+
+_Can be speed-run in 10 minutes!_
+
+## Requirements:
+
+Needs your OpenAPI key..
+
+```
+export OPENAI_API_KEY=# your key here
+```
---
+++ source/app.tsx
@@ -0,0 +1,116 @@
+//@ts-nocheck
+import React, { useState, useEffect } from "react";
+import { Text, Box, useStdout, useInput } from "ink";
+import TextInput from "ink-text-input";
+import { chatFx } from "./effects/chatEffect.js";
+import { useFx } from "@rxfx/react";
+import { trace } from "rxfx";
+
+interface ChatMessageProps {
+  message: Message;
+}
+
+const banner = `
+ ,ggggggggggg,             ,gggggggggggggg
+dP"""88""""""Y8,          dP""""""88""""""
+Yb,  88      \`8b          Yb,_    88
+ \`"  88      ,8P           \`""    88
+     88aaaad8P"                ggg88gggg
+     88""""Yb,       ,gg,   ,gg   88   8,gg,   ,gg
+     88     "8b     d8""8b,dP"    88   d8""8b,dP"
+     88      \`8i   dP   ,88"gg,   88  dP   ,88"
+     88       Yb,,dP  ,dP"Y8,"Yb,,8P,dP  ,dP"Y8,
+     88        Y88"  dP"   "Y8 "Y8P'8"  dP"   "Y8
+`;
+
+const cannedPrompts = [
+  "Who is Sam Altman?",
+  "Your name is Fido",
+  "Bark your name",
+];
+let promptIdx = 0;
+
+export function ChatMessage({ message }: ChatMessageProps) {
+  const isUser = message.role === "user";
+  return (
+    <Box justifyContent={isUser ? "flex-end" : "flex-start"}>
+      <Text>{message.content + "\n"} </Text>
+    </Box>
+  );
+}
+
+export default function App() {
+  const [query, setQuery] = useState("");
+  const { write } = useStdout();
+
+  // 1. TODO Hook up service state as variable 'messages'
+  const messages = [];
+  // 2. TODO show loading/active states
+
+  // 4. Trace logs
+
+  useInput((_, key) => {
+    if (key.escape) {
+      // 3. TODO suppport cancelation, reset
+    }
+
+    if (key.upArrow) {
+      setQuery(cannedPrompts[promptIdx++]); // just an example
+    }
+  });
+
+  function handleSubmit(value) {
+    const userMessage = {
+      id: randomId(),
+      content: value,
+      role: "user",
+      createdAt: new Date(),
+    };
+    setQuery("");
+
+    // 1. TODO Call chat effect with userMessage
+  }
+
+  return (
+    <Box flexDirection="column">
+      <Box>
+        <Text>{banner}</Text>
+      </Box>
+      <Box flexDirection="column">
+        {messages.map((message, idx) => (
+          <ChatMessage key={idx} message={message} />
+        ))}
+      </Box>
+      <Box>
+        <Text>
+          {/* 2. TODO Display Loading, Active states */}
+          {/* 4. TODO Trap and display any error */}
+          Ask the <Text bold>AI</Text>{" "}
+          <Text dimColor> (Esc to cancel, Ctrl-C to quit)</Text>:
+        </Text>
+      </Box>
+      <Box flexDirection="row" borderStyle="double">
+        <Text> &gt; </Text>
+        <TextInput
+          value={query}
+          onChange={(v) => {
+            // 5. TODO Block while answering
+            setQuery(v);
+          }}
+          onSubmit={(value) => {
+            // 5. TODO Block while answering
+            handleSubmit(value);
+          }}
+          width={50}
+          alignSelf="auto"
+        ></TextInput>
+      </Box>
+    </Box>
+  );
+}
+
+function randomId(length = 7) {
+  return Math.floor(Math.pow(2, length * 4) * Math.random())
+    .toString(16)
+    .padStart(length, "0");
+}
